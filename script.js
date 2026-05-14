2c3e461af8be6968b1d4736a07e0f0a0cb47da50
Avery <fusionboltzgx0310@gmail.com>
2026-05-14 18:53:12 -0500

    Added functions for all dogs information

---
+++ script.js
@@ -0,0 +1,83 @@
+function murphyInfo(){
+let info = `Name: Murphy
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function poppyInfo(){
+let info = `Name: Poppy
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function jackInfo(){
+let info = `Name: Jack
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function duffyInfo(){
+let info = `Name: Duffy
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function lucasInfo(){
+let info = `Name: Lucas
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function jakeInfo(){
+let info = `Name: Jake
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function angusInfo(){
+let info = `Name: Angus
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function violetInfo(){
+let info = `Name: Violet
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function piperInfo(){
+let info = `Name: Piper
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function maximusInfo(){
+let info = `Name: Maximus
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function lunaInfo(){
+let info = `Name: Luna
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
+
+function stellaInfo(){
+let info = `Name: Stella
+Breed: N/A
+Adoption Fee: $123.45`
+alert(info)
+}
