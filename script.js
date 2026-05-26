29747b16bd2c63da53daeff2a6d6f5f107f15e75
Avery <fusionboltzgx0310@gmail.com>
2026-05-26 00:54:54 -0500

    Made it so that the inputs into each form get logged into the console

--- script.js
+++ script.js
@@ -94,6 +94,28 @@ priceDisplay.innerText = cart;
 alert("Total: $" + cart);
 });
 
-function submitAlert() {
+function submitCheckoutAlert() {
     alert('Thank you. The form information has been received.');
+
+    console.log('Form Answers');
+    console.log('Name: ' + document.getElementById('name').value);
+    console.log('Email: ' + document.getElementById('email').value);
+    console.log('Street Address: ' + document.getElementById('street').value);
+    console.log('City: ' + document.getElementById('city').value);
+    console.log('Zip Code: ' + document.getElementById('zip').value);
+    console.log('First Time Adopter?: ' + document.querySelector('input[name="first-time"]:checked').value);
+    console.log('Pickup Location: ' + document.getElementById('pickup').value);
+}
+
+function submitContactAlert() {
+    alert('Thank you. The form information has been received.');
+
+    console.log('Form Answers');
+    console.log('First Name: ' + document.getElementById('fname').value);
+    console.log('Last Name: ' + document.getElementById('lname').value);
+    console.log('Email: ' + document.getElementById('email').value);
+    console.log('Phone Number: ' + document.getElementById('phone').value);
+    console.log('Which Age Groups Of Dogs Are They Interested In?: ' + document.querySelectorAll('input[type="checkbox"]:checked'));
+    console.log('Have They Adopted With Us Before?: ' + document.querySelector('input[name="first-time"]:checked').value);
+    console.log('Comment or Message: ' + document.getElementById('message').value);
 }
